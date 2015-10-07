836b7aa1697fe97761cc71eb45a86d918143e461
Timothy M Quast <tquast@math.utexas.edu>
2015-10-07 10:36:20 -0500

    dataframes of stock data!

--- pullData.R
+++ pullData.R
@@ -3,6 +3,28 @@ quote <- function(ids="GOOG", properties=c("n","l1")){
 	id <- paste(ids,collapse=",")
 	prop <- paste(c("&f=",properties),collapse="")
 	url = paste(url,id,prop,"&e=.csv",sep="")
-	read.csv(url,FALSE)
+	x <-read.csv(url,FALSE,col.names=properties)
+	x$time <- Sys.time()
+	x
 
+}
+
+pull <- function(n=100,...){
+	x <- data.frame()
+	print(Sys.time())
+	for(i in 1:n){
+		x = rbind(x,quote(...))
+	}
+	print(Sys.time())
+	x
+}
+
+stripTime <- function(time){
+	x <- as.character(time)
+	x = as.character(x)
+	x = strsplit(x," ")
+	x = x[[1]][2]
+	x = strsplit(x,":")
+	x = as.numeric(x[[1]])
+	x
 }
